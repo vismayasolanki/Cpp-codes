a02565abd6321da1c6b5fa48ba27a1360dd3118f
Ajay Chhajed <ajaychhajed@Ajays-MacBook-Air.local>
2020-10-21 12:48:47 +0530

    adding comments in string.cpp

--- string.cpp
+++ string.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
 #include <cstdio>
+#include<iostream>
 
 using namespace std;
 
-int main(void)//this is for a pull request
+int main(void)
+//this is for a pull request
 {
+    // strings
     string s, s1;
     s = "HELLO";
     s1 = "HELLO";
-    if(s.compare(s1) == 0)
+    if(s.compare(s1) == 0) // comparing string s and s1
         cout << s << " is equal to " << s1 << endl;
-    else
+    else // strings are not equal
         cout << s << " is not equal to " << s1 << endl;
-    s.append(" WORLD!");
+    s.append(" WORLD!"); // appending string "WORLD!"
     cout << s << endl;
     printf("%s\n", s.c_str());
     if(s.compare(s1) == 0)
